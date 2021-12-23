fd59699d347fb7c33b8a74ce76bf6c7290c3c873
Haytam Bakouane <57842491+hbakouane@users.noreply.github.com>
2021-12-24 00:40:21 +0100

    Added Leaderboard page & wallet on header

--- homepage/assets/js/main.js
+++ homepage/assets/js/main.js
@@ -1,47 +1,57 @@
-    window.userWalletAddress = null
-    const loginButton = document.getElementById('loginButton')
-    const userWallet = document.getElementById('userWallet')
+window.userWalletAddress = null
+const loginButton = document.getElementById('loginButton')
+const userWallet = document.getElementById('userWallet')
 
-    function toggleButton() {
-      if (!window.ethereum) {
-        loginButton.innerText = 'MetaMask is not installed'
-        loginButton.classList.remove('bg-purple-500', 'text-white')
-        loginButton.classList.add('bg-gray-500', 'text-gray-100', 'cursor-not-allowed')
-        return false
-      }
+function toggleButton() {
+  if (!window.ethereum) {
+    loginButton.innerText = 'MetaMask is not installed'
+    loginButton.classList.remove('bg-purple-500', 'text-white')
+    loginButton.classList.add('bg-gray-500', 'text-gray-100', 'cursor-not-allowed')
+    return false
+  }
 
-      loginButton.addEventListener('click', loginWithMetaMask)
-    }
+  loginButton.addEventListener('click', loginWithMetaMask)
+}
 
-    async function loginWithMetaMask() {
-      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-        .catch((e) => {
-          console.error(e.message)
-          return
-        })
-      if (!accounts) { return }
+async function loginWithMetaMask() {
+  const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+    .catch((e) => {
+      console.error(e.message)
+      return
+    })
+  if (!accounts) { return }
 
-      window.userWalletAddress = accounts[0]
-      userWallet.innerText = window.userWalletAddress
-      loginButton.innerText = 'Sign out of MetaMask'
+  window.userWalletAddress = accounts[0]
+  userWallet.innerText = window.userWalletAddress
+  // Add a var to the local storage
+  localStorage.setItem('connected_with_metamask', true)
+  loginButton.innerText = 'Sign out of MetaMask'
 
-      loginButton.removeEventListener('click', loginWithMetaMask)
-      setTimeout(() => {
-        loginButton.addEventListener('click', signOutOfMetaMask)
-      }, 200)
-    }
+  loginButton.removeEventListener('click', loginWithMetaMask)
+  setTimeout(() => {
+    loginButton.addEventListener('click', signOutOfMetaMask)
+  }, 200)
+}
 
-    function signOutOfMetaMask() {
-      window.userWalletAddress = null
-      userWallet.innerText = ''
-      loginButton.innerText = 'Sign in with MetaMask'
+function signOutOfMetaMask() {
+  window.userWalletAddress = null
+  userWallet.innerText = ''
+  loginButton.innerText = 'Sign in with MetaMask'
+  localStorage.setItem('connected_with_metamask', false)
+  loginButton.removeEventListener('click', signOutOfMetaMask)
+  setTimeout(() => {
+    loginButton.addEventListener('click', loginWithMetaMask)
+  }, 200)
+}
 
-      loginButton.removeEventListener('click', signOutOfMetaMask)
-      setTimeout(() => {
-        loginButton.addEventListener('click', loginWithMetaMask)
-      }, 200)
-    }
+window.addEventListener('DOMContentLoaded', () => {
+  toggleButton()
+});
 
-    window.addEventListener('DOMContentLoaded', () => {
-      toggleButton()
-    });
+// Sign the user automatically when he's already authenticated with metamask
+let userIsAlreadyAuthenticated = localStorage.getItem('connected_with_metamask')
+if (userIsAlreadyAuthenticated == 'true') {
+  setTimeout(() => {
+    loginButton.click()
+  }, 200)
+}
